7c30d2b65ac68b57e704631c804039feb63b638c
kaan <kaansnl@icloud.com>
2021-01-17 15:45:32 +0300

    fixed the flipped if statements

--- strategies/strategy_2.py
+++ strategies/strategy_2.py
@@ -17,26 +17,27 @@ def strategy(bot):
         ticker_low_day = round(float(ticker["l"][1]), 2)
         average_price = (ticker_low_day + ticker_high_day) / 2
         ticker_current = round(float(ticker["a"][0]), 2)
+        print([ticker_current, " tickercurrent"])
         print(average_price)
-        if average_price < ticker_current:
+        if average_price > ticker_current:
             print("wantu buy")
-            if float(balance[bot.fiat]) > 0.2 / ticker_current:
-                amount_to_buy = balance[bot.fiat] / ticker_current / 100 * bot.risk_management
-                bot.place_order("buy", "limit", ticker_current + 1, amount_to_buy)
+            if float(balance[bot.fiat]) > 0.02 / ticker_current:
+                amount_to_buy = float(balance[bot.fiat]) / ticker_current / 100 * bot.risk_management
+                bot.place_order("buy", "limit", ticker_current, amount_to_buy)
             else:
                 print("no monnai")
 
-        elif average_price > ticker_current:
+        elif average_price < ticker_current:
             print("wantu sell")
-
-            if float(balance[bot.coin]) > 0.2:
+            print(balance[bot.coin])
+            if float(balance[bot.coin]) > 0.02:
                 amount_to_sell = balance[bot.coin]
-                bot.place_order("buy", "limit", ticker_current - 1, amount_to_sell)
+                bot.place_order("sell", "limit", ticker_current - 1, amount_to_sell)
             else:
                 print("no crypto")
 
         else:
-            pass
+            logging.info("current price is exactly the average price of last 24 hours, bruh moment")
 
         time.sleep(4)
 
